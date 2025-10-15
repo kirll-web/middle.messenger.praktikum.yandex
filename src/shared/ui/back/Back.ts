36e88bd1ef7318838e25a358a861888d0562952a
Reghan <reghansan@gmail.com>
2025-10-15 23:10:26 +0300

    Исправил ошибку тестов

--- src/shared/ui/back/Back.ts
+++ src/shared/ui/back/Back.ts
@@ -1,6 +1,6 @@
 import { Block, Router } from '@shared/utils';
 
-import { BackIcon } from '../BackIcon/BackIcon';
+import { BackIcon } from '../BackIcon';
 import template from './Back.hbs?raw';
 
 export class Back extends Block {
